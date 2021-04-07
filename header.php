f973ed0dbc5b7d1469faeb3780d47e021b877c3c
tuckernickman <tucker.nickman@gmail.com>
2021-04-07 16:07:38 -0600

    two font families

--- header.php
+++ header.php
@@ -7,6 +7,14 @@
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />
 
+    <!-- Font Family 1 (retro-sleek-boho) -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Chonburi&family=Lora&family=Oi&display=swap" rel="stylesheet">
+
+    <!-- Font Family 2 (clean-modern-bold) -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Kumbh+Sans:wght@300&family=Train+One&display=swap" rel="stylesheet">
+
 	<!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
