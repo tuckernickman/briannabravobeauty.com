0d0c1688dff3b6b7081d309ccfd454d530d1cd11
tuckernickman <tucker.nickman@gmail.com>
2021-04-08 11:26:25 -0600

    image gallery structure

--- imggallery1.php
+++ imggallery1.php
@@ -1,67 +1,67 @@
-<section>
-    
+<!-- A grid of images -->
+<section> 
     <div class="col section-content perspective">
-        <h2 id="content">2019</h2>
+        <h2 id="content">Brianna Bravo Beauty</h2>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/Wuste.jpg" alt="desert 1">
+                        <img class="img-fluid" src="assets/img/" alt="">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/kraschhh_2.jpg" alt="bones 1">
+                        <img class="img-fluid" src="assets/img/" alt="">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/IMG_3817edit.jpg" alt="2917 edit">
+                        <img class="img-fluid" src="assets/img/" alt="">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/IMG_3556.jpg" alt="IMG 3556">
+                        <img class="img-fluid" src="assets/img/" alt="">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/desert003.jpg" alt="desert 003">
+                        <img class="img-fluid" src="assets/img/2019/" alt="">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/desert001.jpg" alt="desert 001">
+                        <img class="img-fluid" src="assets/img/" alt="">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/bones00072.jpg" alt="bones 2">
+                        <img class="img-fluid" src="assets/img/" alt="">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/1.1.7.jpg" alt="1.1.7">
+                        <img class="img-fluid" src="assets/img/" alt="">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/1.1.5.jpg" alt="1.1.5">
+                        <img class="img-fluid" src="assets/img/" alt="">
                     </div>
                 </a>
             </div>
