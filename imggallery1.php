59057b9f4dc7da5692b78c44e7aa934548e60387
tuckernickman <tucker.nickman@gmail.com>
2021-04-08 18:09:25 -0600

    images inserted into gallery

--- imggallery1.php
+++ imggallery1.php
@@ -5,63 +5,63 @@
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/" alt="">
+                        <img class="img-fluid" src="assets/img/blacklace" alt="model wearing black lace dress, with sleek makeup">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/" alt="">
+                        <img class="img-fluid" src="assets/img/purplebudoire" alt="budoire with purple details">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/" alt="">
+                        <img class="img-fluid" src="assets/img/whitebudoire" alt="budoire with white details">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/" alt="">
+                        <img class="img-fluid" src="assets/img/reddress" alt="look with a red dress">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/2019/" alt="">
+                        <img class="img-fluid" src="assets/img/maternity" alt="maternity makeup photoshoot">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/" alt="">
+                        <img class="img-fluid" src="assets/img/stpatricksday" alt="st. patricks day look">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/" alt="">
+                        <img class="img-fluid" src="assets/img/headshot" alt="headshot">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/" alt="">
+                        <img class="img-fluid" src="assets/img/simplemakeup" alt="simple makeup look">
                     </div>
                 </a>
             </div>
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="assets/img/" alt="">
+                        <img class="img-fluid" src="assets/img/simplewedding" alt="simple wedding makeup">
                     </div>
                 </a>
             </div>
