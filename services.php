245d10268012b9efb30a553094c8b5196a6920ea
RDay52 <74992225+RDay52@users.noreply.github.com>
2021-04-08 19:47:04 -0600

    main

---
+++ services.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,800,300' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="bristyle.css">
+</head>
+
+<body>
+
+<section>
+
+<h1>Services</h1>
+<div class="content">
+       
+     
+       
+        <h2>Layla</h2>
+        <div class="grid">
+          <figure class="effect-layla">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/6.jpg" alt="img06"/>
+            <figcaption>
+              <h2>Crazy <span>Layla</span></h2>
+              <p>When Layla appears, she brings an eternal summer along.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+          <figure class="effect-layla">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/3.jpg" alt="img03"/>
+            <figcaption>
+              <h2>Crazy <span>Layla</span></h2>
+              <p>When Layla appears, she brings an eternal summer along.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+        </div>
+
+        <h2>Roxy</h2>
+        <div class="grid">
+          <figure class="effect-roxy">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
+            <figcaption>
+              <h2>Charming <span>Roxy</span></h2>
+              <p>Roxy was my best friend. She'd cross any border for me.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+          <figure class="effect-roxy">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/1.jpg" alt="img01"/>
+            <figcaption>
+              <h2>Charming <span>Roxy</span></h2>
+              <p>Roxy was my best friend. She'd cross any border for me.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+        </div>
+     
+       
+        <h2>Chico</h2>
+        <div class="grid">
+          <figure class="effect-chico">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
+            <figcaption>
+              <h2>Silly <span>Chico</span></h2>
+              <p>Chico's main fear was missing the morning bus.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+          <figure class="effect-chico">
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/4.jpg" alt="img04"/>
+            <figcaption>
+              <h2>Silly <span>Chico</span></h2>
+              <p>Chico's main fear was missing the morning bus.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+        </div>
+       
+      </div>
+</section>
+
+</body>
+</html>
