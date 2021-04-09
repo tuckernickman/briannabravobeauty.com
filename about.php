ef67c8690fbd49db6b7a48d1633a391b1a412357
tuckernickman <tucker.nickman@gmail.com>
2021-04-08 19:54:26 -0600

    fixed jpgs added images

---
+++ about.php
@@ -0,0 +1,20 @@
+<!-- A grid of images -->
+<section> 
+    <div class="col section-content perspective">
+        <h2 id="content">Brianna Bravo Beauty</h2>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="/briannabravobeauty.com/assets/img/brianna1.jpg" alt="photo of brianna">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="/briannabravobeauty.com/assets/img/brianna2.jpg" alt="photo of brianna">
+                    </div>
+                </a>
+            </div>
+    </div>
+</section>
