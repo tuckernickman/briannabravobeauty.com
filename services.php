e8df55c572e6fb712ce5cca00578987aefb9a112
RDay52 <74992225+RDay52@users.noreply.github.com>
2021-04-13 16:29:36 -0600

    services photo effect

--- services.php
+++ services.php
@@ -1,16 +1,109 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />  
+  <link rel="stylesheet" href="styles/briannastyle.css">
 
+  <!--Java-->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
+    integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous">
+  </script>
 <link href='https://fonts.googleapis.com/css?family=Raleway:400,800,300' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="bristyle.css">
+
 </head>
 
 <body>
-
 <section>
+  <div>
+  <h1 class="text-center">Services I Offer</h1>
+  <p class="text-center">Please note that all services include lash application.</p>
+  </div>
+</section>
 
-<h1>Services</h1>
+<div class="container-fluid contenedor text-center">
+   <h1 class="text-center"></h1>
+   <div class=" container text-center">
+   <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto ">
+         <div class="ver_mas text-center">
+        
+         </div>
+         <article class="text-left">
+            <h2>Bridal<br>$85</h2>
+            <h4>Day of makeup, trials are available at an additional cost. <br>Each bridesmaid is $75</h4>
+         </article>
+         <img src="assets/img/simplewedding.jpg" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
+         <div class="ver_mas text-center">
+            <span id="click" class="lnr lnr-eye"></span>
+         </div>
+         <article class="text-left">
+            <h2>Boudoir <br>$75-$100</h2>
+            <h4>Embrace your inner goddess <br>* simple hair curls or straightened $100</h4>
+         </article>
+         <img src="assets/img/whitebudoire.jpg" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
+         <div class="ver_mas text-center">
+            <span class="lnr lnr-eye"></span>
+         </div>
+         <article class="text-left">
+            <h2>Special Occasions<br>$75</h2>
+            <h4>- Baby Showers <br>- Birthday Parties <br>- Maternity Pictures <br>- Quinceañeras</h4>
+         </article>
+         <img src="assets/img/maternity.jpg" alt="">
+      </div>
+      </div>
+
+      <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
+         <div class="ver_mas text-center">
+            <span id="click" class="lnr lnr-eye"></span>
+         </div>
+         <article class="text-left">
+            <h2>Natural<br>$65</h2>
+            <h4>Softer look enhancing your natural features</h4>
+         </article>
+         <img src="assets/img/simplemakeup1.jpg" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
+         <div class="ver_mas text-center">
+            <span id="click" class="lnr lnr-eye"></span>
+         </div>
+         <article class="text-left">
+            <h2>Fashion/Editorials<br>$75</h2>
+            <h4>Photo shoot, skin focused</h4>
+         </article>
+         <img src="assets/img/reddress.jpg" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
+         <div class="ver_mas text-center">
+            <span id="click" class="lnr lnr-eye"></span>
+         </div>
+         <article class="text-left">
+            <h2>Hair & Makeup Combo<br> $120 ^</h2>
+            <h4>Depending on the complexity of what you would like prices are subject to change.</h4>
+         </article>
+         <img src="assets/img/stpatricksday.jpg" alt="">
+      </div>
+   </div>
+</div>
+</div>
+
+<!--
+  <section>
+
+<h1>Services</h1> 
 <div class="content">
        
      
@@ -18,26 +111,18 @@
         <h2>Layla</h2>
         <div class="grid">
           <figure class="effect-layla">
-<<<<<<< HEAD
-            <img src="simplewedding.jpg" alt="Wedding_Makeup"/>
-=======
-            <img src="/briannabravobeauty.com/assets/img/purplebudoire.jpg" alt="budoire with purple details"/>
->>>>>>> 33f9453b69beee63e2c475d8ff0ef06b62145c9c
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/6.jpg" alt="img06"/>
             <figcaption>
-              <h2> <span>Bridal</span></h2>
-              <p>day of makeup, trials are available at an additional cost  * each bridesmaid is $75</p>
+              <h2>Crazy <span>Layla</span></h2>
+              <p>When Layla appears, she brings an eternal summer along.</p>
               <a href="#">View more</a>
             </figcaption>     
           </figure>
           <figure class="effect-layla">
-<<<<<<< HEAD
-            <img src="whitebudoire.jpg" alt="Budoire_Makeup"/>
-=======
-            <img src="/briannabravobeauty.com/assets/img/whitebudoire.jpg" alt="budoire with white details"/>
->>>>>>> 33f9453b69beee63e2c475d8ff0ef06b62145c9c
+            <img src="whitebudoire.jpg" alt="img03"/>
             <figcaption>
               <h2> <span>Boudoir</span></h2>
-              <p>embrace your inner goddess * simple hair curls or straightened $100</p>
+              <p>embrace your inner goddess * simple hair curls or straightened $100<</p>
               <a href="#">View more</a>
             </figcaption>     
           </figure>
@@ -46,7 +131,7 @@
         <h2>Roxy</h2>
         <div class="grid">
           <figure class="effect-roxy">
-            <img src="/briannabravobeauty.com/assets/img/reddress.jpg" alt="look with a red dress"/>
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
             <figcaption>
               <h2>Charming <span>Roxy</span></h2>
               <p>Roxy was my best friend. She'd cross any border for me.</p>
@@ -54,7 +139,7 @@
             </figcaption>     
           </figure>
           <figure class="effect-roxy">
-            <img src="/briannabravobeauty.com/assets/img/maternity.jpg" alt="maternity makeup photoshoot"/>
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/1.jpg" alt="img01"/>
             <figcaption>
               <h2>Charming <span>Roxy</span></h2>
               <p>Roxy was my best friend. She'd cross any border for me.</p>
@@ -67,7 +152,7 @@
         <h2>Chico</h2>
         <div class="grid">
           <figure class="effect-chico">
-            <img src="/briannabravobeauty.com/assets/img/simplemakeup.jpg" alt="simple makeup look"/>
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
             <figcaption>
               <h2>Silly <span>Chico</span></h2>
               <p>Chico's main fear was missing the morning bus.</p>
@@ -75,7 +160,7 @@
             </figcaption>     
           </figure>
           <figure class="effect-chico">
-            <img src="/briannabravobeauty.com/assets/img/simplewedding.jpg" alt="simple wedding makeup"/>
+            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/4.jpg" alt="img04"/>
             <figcaption>
               <h2>Silly <span>Chico</span></h2>
               <p>Chico's main fear was missing the morning bus.</p>
@@ -86,6 +171,6 @@
        
       </div>
 </section>
-
+-->
 </body>
 </html>
