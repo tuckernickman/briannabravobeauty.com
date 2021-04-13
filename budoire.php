ce56bd50fe403a62d1042dbe4c218f8ca48dea7e
tuckernickman <tucker.nickman@gmail.com>
2021-04-13 17:30:57 -0600

    new pages -maternity, bridal, fashion, budoire

--- budoire.php
+++ budoire.php
@@ -1,24 +1,24 @@
 <section> 
-    <div class="col-sm section-content perspective">
-        <h2 id="content">Fashion and Special Occasion</h2>
+    <div class="col section-content perspective">
+        <h2 id="content">Brianna Bravo Beauty</h2>
             <div id="content" class="col-sm">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="/briannabravobeauty.com/assets/img/stpatricksday.jpg" alt="st. patricks day look">
+                        <img class="img-fluid" src="/briannabravobeauty.com/assets/img/blacklace.jpg" alt="model wearing black lace dress, with sleek makeup">
                     </div>
                 </a>
             </div>
             <div id="content" class="col-sm">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="/briannabravobeauty.com/assets/img/headshot.jpg" alt="headshot">
+                        <img class="img-fluid" src="/briannabravobeauty.com/assets/img/purplebudoire.jpg" alt="budoire with purple details">
                     </div>
                 </a>
             </div>
             <div id="content" class="col-sm">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
-                        <img class="img-fluid" src="/briannabravobeauty.com/assets/img/simplemakeup.jpg" alt="simple makeup look">
+                        <img class="img-fluid" src="/briannabravobeauty.com/assets/img/whitebudoire.jpg" alt="budoire with white details">
                     </div>
                 </a>
             </div>
