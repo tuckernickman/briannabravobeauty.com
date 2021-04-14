552c4dbbecece02cb70a19d94a49d7aeb1c14397
Neam <79028313+nalsamar@users.noreply.github.com>
2021-04-13 19:57:56 -0600

    Add files via upload

---
+++ contact2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<head>
+    <link rel="stylesheet" type="text/css" href="mystyles2.css">
+    <title>Contact Form design</title>
+</head>
+
+    <body>
+
+        <div>
+
+            <h1>Contact Form</h1>
+            <h2>Fill out this form to contact me</h2>
+
+        </div>
+
+        <div class="ocntact-form">
+
+            <form id="contact-form" method="post" action="">
+
+            <input name="name" type="text" class="form-control" placeholder="Your Name" required><br>
+            
+            <input name="email" type="email" class="form-control" placeholder="Your Email" required><br>
+            
+            <textarea name="message" class="form-control" placeholder="Message" rows="4" require><textarea><br>
+
+            <input type="submit" class="form-control submit" value="SEND MESSAGE">
+
+            </form>
+        </div> 
+
+    </body>
+    </html>
