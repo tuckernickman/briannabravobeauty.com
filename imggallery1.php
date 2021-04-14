492c0afe3da53a86b5dd23f58a0a05c48544a74b
tuckernickman <tucker.nickman@gmail.com>
2021-04-13 19:46:06 -0600

    img gallery and index monolith

--- imggallery1.php
+++ imggallery1.php
@@ -1,3 +1,50 @@
+<!-- a carousel of images -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="/assets/img/blacklace.jpg" alt="model wearing black lace dress, with sleek makeup">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/assets/img/purplebudoire.jpg" alt="budoire with purple details">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/assets/img/whitebudoire.jpg" alt="budoire with white details">
+    </div>
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="/assets/img/reddress.jpg" alt="look with a red dress">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/assets/img/maternity.jpg" alt="maternity makeup photoshoot">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/assets/img/whitebudoire.jpg" alt="budoire with white details">
+    </div>
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="/assets/img/blacklace.jpg" alt="model wearing black lace dress, with sleek makeup">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/assets/img/purplebudoire.jpg" alt="budoire with purple details">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/assets/img/whitebudoire.jpg" alt="budoire with white details">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
 <!-- A grid of images -->
 <section> 
     <div class="col section-content perspective">
