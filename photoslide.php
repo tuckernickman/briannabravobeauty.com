73072cc99c5c7c1138155cd5824a2190e7d57f60
RDay52 <74992225+RDay52@users.noreply.github.com>
2021-04-15 20:29:20 -0600

    photo

--- photoslide.php
+++ photoslide.php
@@ -5,7 +5,7 @@
     integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />  
-  <link rel="stylesheet" href="/styles/main.css">
+  <link rel="stylesheet" href="/styles/photo.css">
 
   <!--Java-->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
@@ -26,7 +26,7 @@
 <div class="carousel-container">
  
   
-  <div class="carousel my-carousel carousel--translate col-sm">
+  <div class="carousel my-carousel carousel--translate">
     <ul  class="carousel my-carousel carousel--thumb">
     <input class="carousel__activator" type="radio" name="thumb" id="F" checked="checked"/>
     <input class="carousel__activator" type="radio" name="thumb" id="G"/>
@@ -80,7 +80,61 @@
 </ul>
 </div>
 </div>
+<!--
+<div class="carousel-container">
+  <h2>thumbnail indicators</h2>
+  <ul class="carousel my-carousel carousel--thumb">
+    <input class="carousel__activator" type="radio" id="K" name="thumb" checked="checked"/>
+    <input class="carousel__activator" type="radio" id="L" name="thumb"/>
+    <input class="carousel__activator" type="radio" id="M" name="thumb"/>
+    <input class="carousel__activator" type="radio" id="N" name="thumb"/>
+    <input class="carousel__activator" type="radio" id="O" name="thumb"/>
+    <div class="carousel__controls">
+      <label class="carousel__control carousel__control--backward" for="O"></label>
+      <label class="carousel__control carousel__control--forward" for="L"></label>
+    </div>
+    <div class="carousel__controls">
+      <label class="carousel__control carousel__control--backward" for="K"></label>
+      <label class="carousel__control carousel__control--forward" for="M"></label>
+    </div>
+    <div class="carousel__controls">
+      <label class="carousel__control carousel__control--backward" for="L"></label>
+      <label class="carousel__control carousel__control--forward" for="N"></label>
+    </div>
+    <div class="carousel__controls">
+      <label class="carousel__control carousel__control--backward" for="M"></label>
+      <label class="carousel__control carousel__control--forward" for="O"></label>
+    </div>
+    <div class="carousel__controls">
+      <label class="carousel__control carousel__control--backward" for="N"></label>
+      <label class="carousel__control carousel__control--forward" for="K"></label>
+    </div>
+    <li class="carousel__slide">
+      <h1>K</h1>
+    </li>
+    <li class="carousel__slide">
+      <h1>L</h1>
+    </li>
+    <li class="carousel__slide">
+      <h1>M</h1>
+    </li>
+    <li class="carousel__slide">
+      <h1>N</h1>
+    </li>
+    <li class="carousel__slide">
+      <h1>O</h1>
+    </li>
+    <div class="carousel__indicators">
+      <label class="carousel__indicator" for="K"></label>
+      <label class="carousel__indicator" for="L"></label>
+      <label class="carousel__indicator" for="M"></label>
+      <label class="carousel__indicator" for="N"></label>
+      <label class="carousel__indicator" for="O"></label>
+    </div>
+  </ul>
+</div>
 
+  -->
     
   </ul>
 </div>
