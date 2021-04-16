b8ff44f3caa4364e4b4f40a24756b938adc139ed
tuckernickman <tucker.nickman@gmail.com>
2021-04-15 19:26:06 -0600

    home

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- Website Title -->
+    <title>Brianna Bravo Beauty</title>
+
+    <?php include "header.php"; ?>
+
+</head>
+<body>
+
+    <?php include "menu.php"; ?>
+
+    <!--home page-->
+
+    <?php include "about.php"; ?>
+
+    <?php include "photoslide.php"; ?>
+
+    <a>
+    <style>
+    <?php include "servicesheader.php"; ?>
+    </style>
+    
+    <?php include "services.php"; ?>
+    </a>
+
+    <a>
+    <style>
+    <?php include "contactheader.php"; ?> 
+    </style>
+    
+    <?php include "contact2.php";?>
+    </a>
+    
+    <?php include "footer.php"; ?>
+
+</body>
+</html>
