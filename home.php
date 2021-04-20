3d5b81fc47ea99f24efc8d034f175b0a4ada3b7a
tuckernickman <tucker.nickman@gmail.com>
2021-04-20 17:43:13 -0600

    home

--- home.php
+++ home.php
@@ -13,27 +13,20 @@
     <?php include "menu.php"; ?>
 
     <!--home page-->
-    <a href="about.php">
+    
     <?php include "about.php"; ?>
-    </a>
 
-   
-
-    <a href="services.php">
     <style>
     <?php include "servicesheader.php"; ?>
     </style>
     
     <?php include "services.php"; ?>
-    </a>
 
-    <a style="text-decoration: none">
     <style>
     <?php include "contactheader.php"; ?> 
     </style>
     
     <?php include "contact2.php";?>
-    </a>
 
     <?php include "footer.php"; ?>
 
