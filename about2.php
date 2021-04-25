b53acf7c642a56b416abed0fbdd6e9889643aa2c
tuckernickman <tucker.nickman@gmail.com>
2021-04-25 15:07:03 -0600

    changes

--- about2.php
+++ about2.php
@@ -1,50 +1,6 @@
 <html lang="en">
 <head>
   
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />  
-   <link rel="stylesheet" href="/styles/briannastyle.css">
-  <!--Java-->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
-    integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous">
-  </script>
-<link href='https://fonts.googleapis.com/css?family=Raleway:400,800,300' rel='stylesheet' type='text/css'>
-
-<link rel="stylesheet" href="main.css">
-<link rel="stylesheet" type="text/css" href="mystyles2.css">
-
-<nav class="navbar navbar-expand-lg navbar-light">
-  <div class="container-fluid" id="grad">
-    <a class="navbar-brand" href="index.php">Brianna Bravo Beauty</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-      <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-              <a class="nav-link" href="about.php">About</a>
-          </li>
-          <li class="nav-item active">
-              <a class="nav-link" href="myservices.php">Services</a>
-          </li>
-          <li class="nav-item active">
-              <a class="nav-link" href="Contact.php">Contact</a>
-          </li>
-      </ul>
-      </div>
-    </div>
-  </div>
-</nav>
-
 <style>
   
   .pat{margin-bottom: 5%;
@@ -73,6 +29,8 @@
                     </div>
                 </a>
             </div>
+    </div>
+    <div class="col section-content perspective">
             <div id="content" class="sat">
                 <h1>Hey dollface!</h1>
     <h1 class="pat">  
@@ -84,12 +42,12 @@
         Honey, believe me when I tell you... you are worth so much more than expectations.
         Check out my pricing and feel free to contact me with any questions!
     </h1>
-    
-            </div>
+      </div>
+    </div>
                 
 </section>
 
-    <footer class="footer"> 
+     <!-- <footer class="footer"> 
         <section>
             <a href="mailto:bribravobeauty@gmail.com" class="footer__link">brianna@briannabravobeauty.com</a>
             <link rel="stylesheet" href="styles/main.css">
@@ -116,7 +74,7 @@
                 </ul>
             </section>
             </footer>
-        
+         -->
             <!-- Optional JavaScript -->
             <!-- jQuery first, then Popper.js, then Bootstrap JS -->
             <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
