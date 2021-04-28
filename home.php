c829cb5e3b9819bc627f5a8610572350b9bcd31c
tuckernickman <tucker.nickman@gmail.com>
2021-04-27 20:01:56 -0600

    home

--- home.php
+++ home.php
@@ -18,14 +18,8 @@
 
     <?php include "carousel02.php"; ?>
 
-    <style>
-    <?php include "servicesheader.php"; ?>
-    </style>
     <?php include "services.php"; ?>
 
-    <style>
-    <?php include "contactheader.php"; ?>  
-    </style>
     <?php include "contact2.php";?>
    
     
