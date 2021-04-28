3462da8441bb0f98ab89604ae91d69e93454890f
tuckernickman <tucker.nickman@gmail.com>
2021-04-27 18:08:16 -0600

    carousel02

--- carousel02.php
+++ carousel02.php
@@ -6,11 +6,10 @@
 
 <div class="card-carousel">
   <div class="my-card"></div>
+  <div class="my-card prev"></div>
+  <div class="my-card active"></div>
+  <div class="my-card next"></div>
   <div class="my-card"></div>
-  <div class="my-card"></div>
-  <div class="my-card"></div>
-  <div class="my-card"></div>
-  
 </div>
 
 
