45dc9128b98f322753e28619b9770249765cdbe6
RDay52 <74992225+RDay52@users.noreply.github.com>
2021-04-27 18:22:17 -0600

    services

--- carousel02.php
+++ carousel02.php
@@ -1,7 +1,11 @@
 
 
 
-
+<link rel="stylesheet" href="/styles/carousel02.css">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"
+        crossorigin="anonymous">
+</script>
 
 
 <div class="card-carousel">
@@ -17,4 +21,4 @@
 
 
 
-    <!-- <script src="/js/carousel02.js"></script> -->
+    <script src="/js/carousel02.js"></script>
