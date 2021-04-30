5d8de22315dc4861fd781cc8071c907c6ae111bd
tuckernickman <tucker.nickman@gmail.com>
2021-04-29 18:02:37 -0600

    footer

--- footer2.php
+++ footer2.php
@@ -1,9 +1,6 @@
-<footer>
+<footer class="footer">
   <div class="container pt-5 border-bottom">
     <div class="row">
-        <div class="col-md-3 col-sm-12 mb-3 text-center">
-            <svg class="img-fluid" width="200px" height="60px" viewBox="0 0 200 60" href="assets/logo/logo2.1.svg"></svg>
-        </div>
         <div class="col-md-9 col-sm-12">
         <div class="col-md-3 col-sm-6 col-6 p-0 float-left mb-3">
           <h5 class="mb-4 font-weight-bold text-uppercase">Quick Links</h5>
@@ -16,23 +13,27 @@
         </div>
 
         <div class="col-md-3 col-sm-6 col-6 mb-3 p-0 float-left">
-          <h5 class="mb-4 font-weight-bold text-uppercase">Connect</h5>
+          <h5 class="mb-4 font-weight-bold text-uppercase">Contact Me</h5>
         <ul class="list-group">
           <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-            <a href="https://twitter.com"><i class="fa fa-twitter mr-1"></i> Twitter</a>       
+            <a href="https://twitter.com"><i class="fab fa-twitter mr-1"></i> Twitter</a>       
           </li class="list-group-item bg-transparent border-0 p-0 mb-2">
           <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-            <a href="https://www.facebook.com/bri.briannabeauty" target="_blank"><i class="fa fa-facebook mr-1"></i> Facebook</a>
+            <a href="https://www.facebook.com/bri.briannabeauty" target="_blank"><i class="fab fa-facebook mr-1"></i> Facebook</a>
           </li>
           <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-            <a href="https://www.instagram.com/barefootbeautybybri/" target="_blank"><i class="fa fa-instagram mr-1"></i>Instagram</a>  
+            <a href="https://www.instagram.com/barefootbeautybybri/" target="_blank"><i class="fab fa-instagram mr-1"></i>Instagram</a>  
           </li>
         </ul>
         </div>
-        </div>
-        <div class="col-md-12"><div class="col-md-3 col-sm-6 col-6 p-0 mb-3 float-left">
+        
+        
+        <div class="col-md-3 col-sm-6 col-6 p-0 mb-3 float-left">
             <h6 class="mb-4 font-weight-bold text-uppercase">Pushing the world's expectations of us as womxn!</h6>
-        </div>  
+        </div>
+
+        </div>
+        <div class="col-md-12">
           <div class="py-4 d-flex justify-content-center align-items-center">
                 <h6 class="mb-4 font-weight-bold text-uppercase"> Copyright &copy; 
                     <?php echo date("Y"); ?> 
