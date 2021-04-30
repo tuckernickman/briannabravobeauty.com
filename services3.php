41480cb87656c66377e2b68f48ef82dc528ee33c
tuckernickman <tucker.nickman@gmail.com>
2021-04-30 14:13:00 -0600

    added some alt to the services, fix img size

--- services3.php
+++ services3.php
@@ -12,7 +12,7 @@
 				<h2>Bridal<br>$85</h2>
 				<h4>Day of makeup, trials are available at an additional cost.<br>Each bridesmaid is $75</h4>
 			</article>
-			<img src="/assets/img/simplewedding.jpg" alt="">
+			<img src="/assets/img/simplewedding.jpg" alt="wedding makeup and hair done by Brianna Bravo">
 		</div>
 
 		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
@@ -20,7 +20,7 @@
 				<h2>Boudoir<br>$75-$100</h2>
 				<h4>Embrace your inner goddess <br>* simple hair curls or straightened $100</h4>
 			</article>
-			<img src="/assets/img/whitebudoire.jpg" alt="">
+			<img src="/assets/img/whitebudoire.jpg" alt="Woman in white colored lingere with makeup done by Brianna Bravo">
 		</div>
 
 		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
@@ -28,7 +28,7 @@
 				<h2>Special Occasions<br>$75</h2>
 				<h4>- Baby Showers <br>- Birthday Parties <br>- Maternity Pictures <br>- Quinceañeras</h4>
 			</article>
-			<img src="/assets/img/maternity.jpg" alt="">
+			<img src="/assets/img/maternity.jpg" alt="mother looking stunning in makup look specially made for maternity photoshoot">
 		</div>
 	</div>
 </div>
@@ -41,7 +41,7 @@
 				<h2>Natural<br>$65</h2>
 				<h4>Softer look enhancing your natural features</h4>
 			</article>
-			<img src="/assets/img/simplemakeup.jpg" alt="">
+			<img src="/assets/img/simplemakeup.jpg" alt="simple makeup look enhancing your natural features">
 		</div>
 
 		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
@@ -49,7 +49,7 @@
 				<h2>Hair & Makeup Combo<br>$120+</h2>
 				<h4>Depending on the complexity of what you would like prices are subject to change.</h4>
 			</article>
-			<img src="/assets/img/stpatricksday.jpg" alt="">
+			<img src="/assets/img/stpatricksday.jpg" alt="saint patricks day makeup look, hair and makup combo">
 		</div>
 
 		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 container_foto">
@@ -57,7 +57,7 @@
 				<h2>Fashion/Editorials<br>$75</h2>
 				<h4>Skin focused to evoke a stunning photo shoot</h4>
 			</article>
-			<img src="/assets/img/reddress.jpg" alt="">
+			<img src="/assets/img/reddress.jpg" alt="makeup model wearing red dress at a fashion shoot">
 		</div>
 	</div>
 </div>
