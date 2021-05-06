d8b779bf3ff7a85b18134bdcfa9ffff39119305b
tuckernickman <tucker.nickman@gmail.com>
2021-05-06 17:34:28 -0600

    menu and main css

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid" id="grad">
-          <a class="navbar-brand dark-text" href="index.php">Brianna Bravo Beauty</a>
+          <a class="navbar-brand dark-text" href="home.php">Brianna Bravo Beauty</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon dark-text"></span>
           </button>
