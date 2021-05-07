cf5a7a6c709b8d9fcf02edad86800b4ee0d0c5d0
tuckernickman <tucker.nickman@gmail.com>
2021-05-07 13:28:51 -0600

    edits

--- testimonial.php
+++ testimonial.php
@@ -11,11 +11,11 @@
     <div class="pat col-lg-3 col-md-3 col-sm-6 col-xs-12">
       <p class="dark-text">
         <strong>Grace Flores-Zamora:</strong><br>
-        "I love you as a person,<br> 
-        your soul, vibe, smile, peronality, opinions,<br>
-        and genuinely cared about what I wanted.<br> 
-        You made me feel amazing and beautiful!<br> 
-        You knew what you were doing and I really loved<br> 
+        "I love you as a person, 
+        your soul, vibe, smile, peronality, opinions,
+        and genuinely cared about what I wanted. 
+        You made me feel amazing and beautiful! 
+        You knew what you were doing and I really loved 
         how you chatted and talked through the process!" 
       </p>
     </div>
@@ -25,12 +25,12 @@
     <div class="pat col-lg-3 col-md-3 col-sm-6 col-xs-12">
       <p class="dark-text">
         <strong>Ashley Beal:</strong><br>
-        "You being adorable.<br> 
-        And just being really open, and making me feel really<br>
-        comfortable. Listning to dope music.<br> 
-        I absolutly loved the makeup, especially the liner<br>
-        (cause i can't every do it myself),<br> 
-        And just got all around good vibes from you!"<br>
+        "You being adorable. 
+        And just being really open, and making me feel really
+        comfortable. Listning to dope music. 
+        I absolutly loved the makeup, especially the liner
+        (cause i can't every do it myself), 
+        And just got all around good vibes from you!"
       </p>
     </div>
       
@@ -39,9 +39,9 @@
     <div class="pat col-lg-3 col-md-3 col-sm-6 col-xs-12">
       <p class="dark-text">
         <strong>Michelle Antionetta Crossley:</strong><br>
-        "You being so easy to talk too,<br> 
+        "You being so easy to talk too, 
         with being sassy as well,
-        but in a good way.<br> 
+        but in a good way. 
         Plus making me feel so comfortable!"
       </p>
     </div>
@@ -51,10 +51,13 @@
     <div class="pat col-lg-3 col-md-3 col-sm-6 col-xs-12">
       <p class="dark-text"> 
         <strong>Jazalynn Powers:</strong><br>
-        "You were so sweet and welcoming even<br>
-        through I was SO LATE. You immediately got me talking and helped me feel so comfortable.<br>
-        You are a makeup magician and wish I could do makeup like you!!!<br>
-        My hair was also fire! It stresses me the hell out when other people do my hair, but you did just amazing!""
+        "You were so sweet and welcoming even
+        through I was SO LATE. 
+        You immediately got me talking and helped me feel so comfortable.
+        You are a makeup magician and wish I could do makeup like you!!!
+        My hair was also fire! 
+        It stresses me the hell out when other people do my hair, 
+        but you did just amazing!"
       </p>
     </div>
 
