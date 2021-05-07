c7341da0fb49913b05fd2d53ea72827cd1ba00ba
tuckernickman <tucker.nickman@gmail.com>
2021-05-07 11:39:44 -0600

    edits

--- testimonial.php
+++ testimonial.php
@@ -61,71 +61,60 @@
         <h6 class="mat">Makeup Artist</h6>
 </div>
           
-<div style="width:150%">
-  <div class="pat">"<strong>Grace Flores-Zamora-</strong> I love you as a person,<br> your soul, vibe, smile, peronality, opinions,<br>
-  and genuinely cared about what I wanted.<br> You made me feel amazing and beautiful!<br> You knew
-  what you were doing and I really loved<br> how you chatted and talked through the process!"" 
+<div class="container">
+<div class="pat">
+  <p class="dark-text">
+    <strong>Grace Flores-Zamora-</strong> 
+    "I love you as a person,<br> 
+    your soul, vibe, smile, peronality, opinions,<br>
+    and genuinely cared about what I wanted.<br> 
+    You made me feel amazing and beautiful!<br> 
+    You knew what you were doing and I really loved<br> 
+    how you chatted and talked through the process!" 
+  </p>
 </div>
 
     
   
-    <div class="pat">
-    "<strong>Ashley Beal-</strong> You being adorable.<br> And just being really open, and making me feel really<br>
-    comfortable. Listning to dope music.<br> I absolutly loved the makeup, especially the liner<br>
-    (cause i can't every do it myself),<br> And just got all around good vibes from you!"<br>
+<div class="pat">
+  <p class="dark-text">
+    <strong>Ashley Beal-</strong> 
+    "You being adorable.<br> 
+    And just being really open, and making me feel really<br>
+    comfortable. Listning to dope music.<br> 
+    I absolutly loved the makeup, especially the liner<br>
+    (cause i can't every do it myself),<br> 
+    And just got all around good vibes from you!"<br>
+  </p>
 </div>
    
 
    
-    <div class="pat">
-    "<strong>Michelle Antionetta Crossley-</strong>You being so easy to talk too,<br> with being sassy as well
-    but in a good way.<br> Plus making me feel so comfortable!"
+<div class="pat">
+  <p class="dark-text">
+    <strong>Michelle Antionetta Crossley-</strong>
+    "You being so easy to talk too,<br> 
+    with being sassy as well
+    but in a good way.<br> 
+    Plus making me feel so comfortable!"
+  </p>
 </div>
   
 
   
-    <div class="pat"> 
-    "<strong>Jazalynn Powers-</strong> You were so sweet and welcoming even<br>
+<div class="pat">
+  <p class="dark-text"> 
+    <strong>Jazalynn Powers-</strong> 
+    "You were so sweet and welcoming even<br>
     through I was SO LATE. You Immediately got me taking and helped me feel so comfortable.<br>
     You are a makeup magician and i wish i could do makeup like you!!!<br>
     My hair was also fire it stesses me the hell out to have other people do my hair but you did just amazing!""
+  </p>
 </div>
+
 </div>
 </section>
-  
-<br>
 
-    <footer class="footer"> 
-        <section>
-            <a href="mailto:bribravobeauty@gmail.com" class="footer__link">brianna@briannabravobeauty.com</a>
-            <link rel="stylesheet" href="styles/main.css">
-                <ul class="social-list">
-                    
-                    
-                    <li class="social-list__item">
-                        <a class="social-list__link" href="https://twitter.com">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                    </li>
-                   
-                    <li class="social-list__item">
-                        <a class="social-list__social-link" href="https://www.facebook.com" target="_blank">
-                            <i class="fab fa-facebook"></i>
-                        </a>
-                    </li>
-                
-                    <li class="social-list__item">
-                        <a class="social-list__social-link" href="https://www.instagram.com" target="_blank">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                    </li>
-                </ul>
-            </section>
-            
-            <script>
-              AOS.init();
-            </script>
-            
 </body>
 
 </html>
