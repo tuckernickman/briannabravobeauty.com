5d2d079d29ac03d54ba570d4e00d8d34f30723d7
tuckernickman <tucker.nickman@gmail.com>
2021-05-07 13:23:15 -0600

    edits

--- testimonial.php
+++ testimonial.php
@@ -1,67 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<body>
-<section>
-<div>
-        <h1 class="mat dark-text">Brianna Bravo</h1>
+<body>  <section>
+    <div>
+            <h1 class="mat dark-text">Brianna Bravo</h1>
+            <h6 class="mat dark-text">Makeup Artist</h6>
+    </div>
+              
+    <div class="container">
+    <div class="pat col-lg-3 col-md-3 col-sm-6 col-xs-12">
+      <p class="dark-text">
+        <strong>Grace Flores-Zamora:</strong><br>
+        "I love you as a person,<br> 
+        your soul, vibe, smile, peronality, opinions,<br>
+        and genuinely cared about what I wanted.<br> 
+        You made me feel amazing and beautiful!<br> 
+        You knew what you were doing and I really loved<br> 
+        how you chatted and talked through the process!" 
+      </p>
+    </div>
 
-        <h6 class="mat dark-text">Makeup Artist</h6>
-</div>
-          
-<div class="container">
-<div class="pat col-lg-4 col-md-4 col-sm-6 col-xs-12">
-  <p class="dark-text">
-    <strong>Grace Flores-Zamora-</strong> 
-    "I love you as a person,<br> 
-    your soul, vibe, smile, peronality, opinions,<br>
-    and genuinely cared about what I wanted.<br> 
-    You made me feel amazing and beautiful!<br> 
-    You knew what you were doing and I really loved<br> 
-    how you chatted and talked through the process!" 
-  </p>
-</div>
+        
+      
+    <div class="pat col-lg-3 col-md-3 col-sm-6 col-xs-12">
+      <p class="dark-text">
+        <strong>Ashley Beal:</strong><br>
+        "You being adorable.<br> 
+        And just being really open, and making me feel really<br>
+        comfortable. Listning to dope music.<br> 
+        I absolutly loved the makeup, especially the liner<br>
+        (cause i can't every do it myself),<br> 
+        And just got all around good vibes from you!"<br>
+      </p>
+    </div>
+      
 
-    
-  
-<div class="pat col-lg-4 col-md-4 col-sm-6 col-xs-12">
-  <p class="dark-text">
-    <strong>Ashley Beal-</strong> 
-    "You being adorable.<br> 
-    And just being really open, and making me feel really<br>
-    comfortable. Listning to dope music.<br> 
-    I absolutly loved the makeup, especially the liner<br>
-    (cause i can't every do it myself),<br> 
-    And just got all around good vibes from you!"<br>
-  </p>
-</div>
-   
+      
+    <div class="pat col-lg-3 col-md-3 col-sm-6 col-xs-12">
+      <p class="dark-text">
+        <strong>Michelle Antionetta Crossley:</strong><br>
+        "You being so easy to talk too,<br> 
+        with being sassy as well,
+        but in a good way.<br> 
+        Plus making me feel so comfortable!"
+      </p>
+    </div>
+      
 
-   
-<div class="pat col-lg-4 col-md-4 col-sm-6 col-xs-12">
-  <p class="dark-text">
-    <strong>Michelle Antionetta Crossley-</strong>
-    "You being so easy to talk too,<br> 
-    with being sassy as well
-    but in a good way.<br> 
-    Plus making me feel so comfortable!"
-  </p>
-</div>
-  
+      
+    <div class="pat col-lg-3 col-md-3 col-sm-6 col-xs-12">
+      <p class="dark-text"> 
+        <strong>Jazalynn Powers:</strong><br>
+        "You were so sweet and welcoming even<br>
+        through I was SO LATE. You immediately got me talking and helped me feel so comfortable.<br>
+        You are a makeup magician and wish I could do makeup like you!!!<br>
+        My hair was also fire! It stresses me the hell out when other people do my hair, but you did just amazing!""
+      </p>
+    </div>
 
-  
-<div class="pat col-lg-4 col-md-4 col-sm-6 col-xs-12">
-  <p class="dark-text"> 
-    <strong>Jazalynn Powers-</strong> 
-    "You were so sweet and welcoming even<br>
-    through I was SO LATE. You Immediately got me taking and helped me feel so comfortable.<br>
-    You are a makeup magician and i wish i could do makeup like you!!!<br>
-    My hair was also fire it stesses me the hell out to have other people do my hair but you did just amazing!""
-  </p>
-</div>
+    </div>
+  </section>
 
-</div>
-</section>
 
 </body>
 
