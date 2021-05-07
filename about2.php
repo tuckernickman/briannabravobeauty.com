4c8cc9c699e03d54fce22eedcb7de9058b116ec2
RDay52 <74992225+RDay52@users.noreply.github.com>
2021-05-06 20:19:35 -0600

    about

--- about2.php
+++ about2.php
@@ -1,72 +1,27 @@
 <html lang="en">
 <head>
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />  
-   <link rel="stylesheet" href="/styles/briannastyle.css">
-  <!--Java-->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
-    integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous">
-  </script>
-<link href='https://fonts.googleapis.com/css?family=Raleway:400,800,300' rel='stylesheet' type='text/css'>
-
-<link rel="stylesheet" href="main.css">
-<link rel="stylesheet" type="text/css" href="mystyles2.css">
-
-<nav class="navbar navbar-expand-lg navbar-light">
-  <div class="container-fluid" id="grad">
-    <a class="navbar-brand" href="index.php">Brianna Bravo Beauty</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-      <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-              <a class="nav-link" href="about.php">About</a>
-          </li>
-          <li class="nav-item active">
-              <a class="nav-link" href="myservices.php">Services</a>
-          </li>
-          <li class="nav-item active">
-              <a class="nav-link" href="Contact.php">Contact</a>
-          </li>
-      </ul>
-      </div>
-    </div>
-  </div>
-</nav>
-
+  
 <style>
   
   .pat{
  
   font-size:large; 
   font-family: 'Lobster', cursive;
-  line-height: 152%;
+  line-height: 200%;
 
     }
   
   .sat{
   margin: 9%;
-  background: linear-gradient(#ffffff, #ffe8d6);
+  background: linear-gradient(#986572, #986572);
   
-  background-color:#ffff;
+  background-color:#986572;
   width: 600px;
   padding: 15px;
   margin: 20px;
-  background: #fff;
+  background: #986572;
   
-  background: linear-gradient(to right, white, transparent 70%);
+  background: linear-gradient(to right, #986572, transparent 75%);
   
   font-size:large;
   font-family: 'Lobster', cursive;
@@ -79,15 +34,7 @@
 <body>
     
   <section> 
-    <div class="col section-content perspective">
-        <h2>Brianna Bravo Beauty</h2>
-            <div id="content" class="col-sm">
-                <a href="" class="field-group-link project-link d-block p-visible">
-                    <div class="project-img bg">
-                        <img class="img-fluid" src="/assets/img/brianna1.jpg" alt="photo of brianna">
-                    </div>
-                </a>
-            </div>
+
             <div id="content" class="sat">
                 <h1>Hey dollface!</h1>
     <h1 class="pat">  
@@ -102,40 +49,7 @@
     
             </div>
                 
-</section>
-
-    <footer class="footer"> 
-        <section>
-            <a href="mailto:bribravobeauty@gmail.com" class="footer__link">brianna@briannabravobeauty.com</a>
-            <link rel="stylesheet" href="styles/main.css">
-                <ul class="social-list">
-                    
-                    
-                    <li class="social-list__item">
-                        <a class="social-list__link" href="https://twitter.com">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                    </li>
-                   
-                    <li class="social-list__item">
-                        <a class="social-list__social-link" href="https://www.facebook.com" target="_blank">
-                            <i class="fab fa-facebook"></i>
-                        </a>
-                    </li>
-                
-                    <li class="social-list__item">
-                        <a class="social-list__social-link" href="https://www.instagram.com" target="_blank">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                    </li>
-                </ul>
-            </section>
-            </footer>
         
-            <!-- Optional JavaScript -->
-            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+            
 </body>
 </html>
