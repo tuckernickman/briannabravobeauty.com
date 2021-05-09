dae3c6aad0750eb4efd0dbf68a96afbc71779123
tuckernickman <tucker.nickman@gmail.com>
2021-05-09 14:39:39 -0600

    edits

--- about.php
+++ about.php
@@ -32,8 +32,8 @@
 
 
 
-<div class="card container mb-3" >
-  <div class="container g-0">
+<div class="container-fluid mb-3" >
+  <div class="card container g-0">
 
     <div id="_cf" class="container col-lg-4 col-md-4 col-sm-6 col-xs-12">
       <div id="cf" class="container align-image">
