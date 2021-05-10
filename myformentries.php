24953ace765cfbac93204568591640cbc5ad03a8
tuckernickman <tucker.nickman@gmail.com>
2021-05-10 09:37:25 -0600

    trying to get somewhere

--- myformentries.php
+++ myformentries.php
@@ -16,3 +16,29 @@
 			</div>
 		</div>
 </section>
+
+<?php 
+try {
+    $conn = get_db_connection();
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT id, name, phone, email, yesnoRadio, comment FROM bbeauty_table")
+
+echo "table";
+echo "<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+<tr>
+    <td><?php echo $row[id]; ?></td>
+    <td><?php echo $row[name]; ?></td>
+    <td><?php echo $row[phone]; ?></td>
+    <td><?php echo $row[email]; ?></td>
+    <td><?php echo $row[yesnoRadio]; ?></td>
+    <td><?php echo $row[comment]; ?></td>
+</tr>
+<?php }
+echo "</table>";
+}
+catch (PDOException $e) {
+    echo "Error: " . $ee->getMessage();
+}
+$conn = null;
+?>
