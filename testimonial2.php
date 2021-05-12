9b9b2a7fdc0e492d4f7a674fce4f6b507c431400
tuckernickman <tucker.nickman@gmail.com>
2021-05-12 09:16:26 -0600

    added col... sizing

--- testimonial2.php
+++ testimonial2.php
@@ -45,43 +45,64 @@
     <div class="container">
         
         <div class="row">
-          <div class="col"
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"
           data-aos="flip-left"
      data-aos-easing="ease-out-cubic"
      data-aos-duration="1500">
-            <div class="pat"><strong>Grace Flores-Zamora:</strong> <br> I love you as a person, your soul, vibe, smile, peronality, opinions,
-                and genuinely cared about what I wanted. You made me feel amazing and beautiful You knew what you were doing and I really loved how you chatted and talked through the process!
-              </div>
+            <div class="pat"><strong>Grace Flores-Zamora:</strong> <br>
+            "I love you as a person, 
+            your soul, vibe, smile, peronality, opinions,
+            and genuinely cared about what I wanted. 
+            You made me feel amazing and beautiful! 
+            You knew what you were doing and I really loved 
+            how you chatted and talked through the process!" 
+            </div>
           </div>
 
-          <div class="col"
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"
           data-aos="flip-left"
      data-aos-easing="ease-out-cubic"
      data-aos-duration="2000"
      data-aos-offset="300">
             <div class="pat">
-                <strong>Ashley Beal:</strong> <br> You being adorable. And just being really open, and making me feel really comfortable. Listning to dope music. I absolutly loved the makeup, especially the liner (cause i can't every do it myself), and just got all around good vibes from you!
+                <strong>Ashley Beal:</strong> <br>
+            "You being adorable. 
+            And just being really open, and making me feel really
+            comfortable. Listning to dope music. 
+            I absolutly loved the makeup, especially the liner
+            (cause I can't every do it myself), 
+            And just got all around good vibes from you!"
             </div>
           </div>
 
-          <div class="col"
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"
           data-aos="flip-left"
      data-aos-easing="ease-out-cubic"
      data-aos-duration="2500"
      data-aos-offset="400">
             <div class="pat">
-                <strong>Michelle Antionetta Crossley:</strong> <br> You being so easy to talk too, with being sassy as well but in a good way. Plus making me feel so comfortable!
+                <strong>Michelle Antionetta Crossley:</strong> <br>
+            "You being so easy to talk to, 
+            and sassy as well,
+            but in a good way. 
+            Plus making me feel so comfortable!"
             </div>
           </div>
 
-          <div class="col"
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12"
           data-aos="flip-left"
      data-aos-easing="ease-out-cubic"
      data-aos-duration="3000"
      data-aos-offset="500">
             <div class="pat"> 
-                <strong>Jazalynn Powers:</strong> <br> You were so sweet and welcoming even
-                through I was SO LATE. You Immediately got me taking and helped me feel so comfortable.You are a makeup magician and i wish i could do makeup like you!!! My hair was also fire it stesses me the hell out to have other people do my hair but you did just amazing!
+                <strong>Jazalynn Powers:</strong> <br>
+            "You were so sweet and welcoming even
+            through I was SO LATE. 
+            You immediately got me talking and helped me feel so comfortable.
+            You are a makeup magician and wish I could do makeup like you!!!
+            My hair was also fire! 
+            It stresses me the hell out when other people do my hair, 
+            but you did just amazing!"
             </div>
           </div>
           </div>
