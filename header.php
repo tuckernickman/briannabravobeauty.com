bf871b84c2b4c6bd4d0b8f9cbf5cc462e7a1809c
tuckernickman <tucker.nickman@gmail.com>
2021-05-11 20:58:00 -0600

    header

--- header.php
+++ header.php
@@ -31,7 +31,5 @@
   
     <link rel="stylesheet" type="text/css" href="/styles/about.css">
     <link rel="stylesheet" type="text/css" href="/styles/carousel.css">
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     
 </head>
