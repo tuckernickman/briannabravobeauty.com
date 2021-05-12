8c7f67c1bf18849ee1166e7917ccd95a71ac7cea
Neam <79028313+nalsamar@users.noreply.github.com>
2021-05-11 20:46:57 -0600

    Add files via upload

---
+++ testimonial2.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "header.php"; ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+    
+
+    
+
+<style>
+ 
+ .pat {
+  background-color:#986572 ;
+  background: #986572;
+  background: linear-gradient(to right, #986572, transparent 70%); 
+  
+ 
+  box-shadow: 5px 0 7px 0;
+  box-shadow: rgba(59, 58, 58, 0.39);
+
+
+  margin: 2%;
+  padding: 3%;
+
+  min-height: 97%;
+  min-width: 100%;
+ }
+ 
+</style> 
+
+</head>
+<body>
+
+
+<section>
+
+    <div>
+        <h1><strong>Testimonials</strong></h1>
+    </div>
+
+    <div class="container">
+        
+        <div class="row">
+          <div class="col"
+          data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="1500">
+            <div class="pat"><strong>Grace Flores-Zamora:</strong> <br> I love you as a person, your soul, vibe, smile, peronality, opinions,
+                and genuinely cared about what I wanted. You made me feel amazing and beautiful You knew what you were doing and I really loved how you chatted and talked through the process!
+              </div>
+          </div>
+
+          <div class="col"
+          data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2000"
+     data-aos-offset="300">
+            <div class="pat">
+                <strong>Ashley Beal:</strong> <br> You being adorable. And just being really open, and making me feel really comfortable. Listning to dope music. I absolutly loved the makeup, especially the liner (cause i can't every do it myself), and just got all around good vibes from you!
+            </div>
+          </div>
+
+          <div class="col"
+          data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="2500"
+     data-aos-offset="400">
+            <div class="pat">
+                <strong>Michelle Antionetta Crossley:</strong> <br> You being so easy to talk too, with being sassy as well but in a good way. Plus making me feel so comfortable!
+            </div>
+          </div>
+
+          <div class="col"
+          data-aos="flip-left"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="3000"
+     data-aos-offset="500">
+            <div class="pat"> 
+                <strong>Jazalynn Powers:</strong> <br> You were so sweet and welcoming even
+                through I was SO LATE. You Immediately got me taking and helped me feel so comfortable.You are a makeup magician and i wish i could do makeup like you!!! My hair was also fire it stesses me the hell out to have other people do my hair but you did just amazing!
+            </div>
+          </div>
+          </div>
+          </div>
+   
+</section>
+ 
+
+
+       
+            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+            
+            <script type="text/javascript" src="animate.js"></script>
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+              <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+          
+    	
+          <script>
+            $(function() {
+                 AOS.init();
+            });
+       </script>
+</body>
+
+</html>
+
+
